863a001e3d4d77ae254372d65558f71158db72fd
njacob1001 <njacob1001@gmail.com>
2021-05-02 18:34:31 -0500

    Initial Commit

---
+++ coffe-mockup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coffe-mockup
+//
+//  Created by jacob on 2/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coffe-mockup/coffe_mockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffe_mockupApp.swift
+//  coffe-mockup
+//
+//  Created by jacob on 2/05/21.
+//
+
+import SwiftUI
+
+@main
+struct coffe_mockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
